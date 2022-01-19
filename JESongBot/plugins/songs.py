683f9cd74bb4d17267b9e5e52fa0dde87136c1df
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-19 15:11:43 +0530

    Update songs.py

--- JESongBot/plugins/songs.py
+++ JESongBot/plugins/songs.py
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("⚙️ Converting to MP3 Audio 🎵...\n🌷 Wait 1 Min\nــ٨ـہہـ♡ـ٨ـہــــ٨ـہہـ♡ـ٨ـ")
+    m.edit("ــ٨ـہہـ♡ـ٨ـہــــ٨ـہہـ♡ـ٨ـ\n⚙️ Converting to MP3 Audio 🎵...\n🌷 Wait 1 Min\nــ٨ـہہـ♡ـ٨ـہــــ٨ـہہـ♡ـ٨ـ")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = 'successfully uploaded ✅\n◇───────────────◇\n\n[🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)\n◇───────────────◇\n\n©️ 2022 | [🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)| ــ٨ـہہـ♡ـ٨'
+        rep = 'successfully uploaded ✅\n◇───────────────◇\n\n[🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)\n\n◇───────────────◇\n\n©️ 2022 | [🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)| ــ🌷٨ـہہـ♡ـ🌷٨'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
