20a4d112fdd498e725f1de560a1343a0913991d6
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-19 15:27:46 +0530

    Update songs.py

--- JESongBot/plugins/songs.py
+++ JESongBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '🌷 *successfully uploaded* ✅\n\n◇───────────────◇\n[🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)\n◇───────────────◇\n\n©️ 2022 | [🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)| 🎼💕🎼'
+        rep = '<b>🌷 successfully uploaded ✅</b>\n\n◇───────────────◇\n[🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)\n◇───────────────◇\n\n©️ 2022 | [🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)| 🎼💕🎼'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
